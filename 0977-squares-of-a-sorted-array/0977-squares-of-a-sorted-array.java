92b2e157d09dd979b2a8bbd00e81e05a4929a12c
Krishnapalsingh03 <krishnasingh132431@gmail.com>
2026-07-16 01:04:46 +0530

    Update 0977-squares-of-a-sorted-array.java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int []newarray= new int[nums.length];
-        int max=0;
+        
         for(int i =0;i<nums.length;i++){
             nums[i]=nums[i]*nums[i];
          newarray[i]=nums[i];
