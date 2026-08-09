4a374d10d4d6de67de61231b3afc84f90cb571ee
Krishnapalsingh03 <krishnasingh132431@gmail.com>
2026-08-10 02:18:20 +0530

    Time: 8 ms (24.45%), Space: 45.6 MB (68.65%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int countU=0;
+        int countD=0;
+        int countR=0;
+        int countL=0;
+        for(int i=0;i<moves.length();i++){
+              if(moves.charAt(i)=='U'){
+                countU++;
+              }
+              else if(moves.charAt(i)=='D'){
+                countD++;
+              }
+               else if(moves.charAt(i)=='R'){
+                countR++;
+              }
+              else if(moves.charAt(i)=='L'){
+                countL++;
+              }
+        }
+        if(countU==countD&&countR==countL){
+            return true;
+        }
+        return false;
+      
+    }
+}
